324303b817f7bb4ec8261beb7c8aa9f942b4ef9f
Jomon K Joy <jomonkurianjoy@gmail.com>
2017-11-25 17:53:53 +0000

    updated addressing with ADDR_LSB

--- asymmetric_ram.sv
+++ asymmetric_ram.sv
@@ -12,7 +12,8 @@ module byte_write_sdp_ram #(
   output logic [DATA_WIDTH-1:0]   rd_data
 );
   
-  logic [7:0][DATA_WIDTH/8-1:0] mem [2**ADDR_WIDTH];
+  localparam ADDR_LSB = $clog2(DATA_WIDTH/8);
+  logic [7:0][DATA_WIDTH/8-1:0] mem [2**(ADDR_WIDTH-ADDR_LSB)];
   
   logic                    wr_enable_r;
   logic [ADDR_WIDTH-1:0]   wr_address_r;
@@ -25,23 +26,23 @@ module byte_write_sdp_ram #(
     wr_address_r <= wr_address;
     wr_data_r <= wr_data;
     wr_strb_r <= wr_strb;
-    rd_data_r <= mem[wr_address];
+    rd_data_r <= mem[wr_address[ADDR_WIDTH-1:ADDR_LSB]];
   end
   integer i;
   always_ff @(posedge wr_clk) begin
     if (wr_enable_r) begin
       for (i=0; i<DATA_WIDTH/8; i++) begin
         if (wr_strb_r[i]) begin
-          mem[wr_address_r][i] <= wr_data_r[8*i+:8];
+          mem[wr_address_r[ADDR_WIDTH-1:ADDR_LSB]][i] <= wr_data_r[8*i+:8];
         end else begin
-          mem[wr_address_r][i] <= rd_data_r[8*i+:8];
+          mem[wr_address_r[ADDR_WIDTH-1:ADDR_LSB]][i] <= rd_data_r[8*i+:8];
         end
       end
     end
   end
   
   always_ff @(posedge rd_clk) begin
-    rd_data <= mem[rd_address];
+    rd_data <= mem[rd_address[ADDR_WIDTH-1:ADDR_LSB]];
   end
   
 endmodule
