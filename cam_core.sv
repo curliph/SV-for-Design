a45a7417f6c0e07c0c27020a40b203f81040ee66
Jomon K Joy <jomonkurianjoy@gmail.com>
2018-01-14 07:44:53 +0000

    Update cam_core.sv

--- cam_core.sv
+++ cam_core.sv
@@ -15,6 +15,24 @@ module cam_core #(
   output logic                  lookup_hit
 );
 
-
-
+  localparam ADDR_DEPTH = 2**ADDR_WIDTH;
+  logic [DATA_WIDTH-0:0] lookup_table [ADDR_DEPTH];
+  logic [ADDR_DEPTH-1:0] lookup_hit_next;
+  
+  always_ff @(posedge clk) begin
+    if (cam_we) begin
+      lookup_table[cam_addr] <= {cam_valid,cam_data};
+    end
+  end
+  
+  genvar i;
+  generate for (i=0; i<ADDR_DEPTH; i++) begin : lookup_gen
+    assign lookup_hit_next[i] = lookup_table[i] == {1'b1,lookup_data};
+  end endgenerate
+  
+  always_ff @(posedge clk) begin
+    lookup_addr <= onehot2bin(lookup_hit_next);
+    lookup_hit  <= |lookup_hit_next;
+  end
+  
 endmodule
