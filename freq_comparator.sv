84de60e98778de218920cb491c5fcfab16206f7c
Jomon K Joy <jomonkurianjoy@gmail.com>
2018-02-09 13:45:24 +0000

    Update freq_comparator.sv

--- freq_comparator.sv
+++ freq_comparator.sv
@@ -23,7 +23,7 @@ module freq_comparator #(
     .clk             (aclk),
     .reset           (areset),
     .enable          (enable),
-    .count_enable    (count_enable),
+    .count_enable    (count_enable), // flop this signal so as to transfer to bdomin
     .timer_done      (timer_done_sync),
     .timeout_value   (timeout_value),
     .timeout_compare (btimeout_count),
@@ -56,7 +56,7 @@ module freq_comparator #(
     .reset         (breset),
     .enable        (count_enable_sync),
     .timeout_count (btimeout_count),
-    .timer_done    (timer_done)
+    .timer_done    (timer_done) // flop this signal so as to transfer to adomin
   );
   
 endmodule
@@ -68,7 +68,7 @@ module freq_comparator_bdomain #(
   input  logic reset,
   input  logic enable,
   output logic [DATA_WIDTH-1:0] timeout_count,
-  output logic timer_done
+  output logic timer_done // flop this signal so as to transfer to adomin
 );
   
   logic enable_d;
@@ -87,6 +87,69 @@ module freq_comparator_bdomain #(
       timer_done <= 1'b0;
     end else if(enable_d & ~enable) begin
       timer_done <= 1'b1;
+    end
+  end
+  
+endmodule
+
+module freq_comparator_adomain #(
+  parameter DATA_WIDTH = 32
+) (
+  input  logic clk,
+  input  logic reset,
+  input  logic enable,
+  output logic count_enable, // flop this signal so as to transfer to bdomin
+  input  logic timer_done,
+  input  logic [DATA_WIDTH-1:0] timeout_value,
+  input  logic [DATA_WIDTH-1:0] timeout_compare,
+  output logic [DATA_WIDTH-1:0] timeout_count,
+  output logic compare_ge,
+  output logic compare_done
+);
+  
+  typedef enum logic [1:0] {IDLE,ACTIVE,DONE}state_type;
+  state_type state;
+  
+  always_ff @(posedge clk) begin
+    if (reset | (state == IDLE && enable)) begin
+      timeout_count <= 'd0;
+    end else if(count_enable) begin
+      timeout_count <= timeout_count + 1'b1;
+    end
+  end
+  always_ff @(posedge clk) begin
+    if (state == DONE && timer_done) begin
+      compare_ge <= timeout_count >= timeout_compare;
+    end
+  end
+  always_ff @(posedge clk) begin
+    if (reset) begin
+      state <= IDLE;
+      count_enable <= 1'b0;
+      compare_done <= 1'b0;
+    end else begin
+      case(state)
+        IDLE : begin
+          if (enable) begin
+            state <= ACTIVE;
+            count_enable <= 1'b1;
+            compare_done <= 1'b0;
+          end
+        end
+        ACTIVE : begin
+          if (timeout_count >= timeout_value) begin
+            state <= DONE;
+            count_enable <= 1'b0;
+          end
+        end
+        DONE : begin
+          if (timer_done) begin
+            state <= IDLE;
+            compare_done <= 1'b1;
+          end
+        end
+        default : state <= IDLE;
+      endcase
     end
   end
   
