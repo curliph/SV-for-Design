a68c45ccb61afc68c5675fde33248095544f8c09
Jomon K Joy <jomonkurianjoy@gmail.com>
2018-02-14 04:18:59 +0000

    Update watchdog_timer.sv

--- watchdog_timer.sv
+++ watchdog_timer.sv
@@ -7,6 +7,21 @@ module watchdog_timer #(
   input  logic [TIMR_WIDTH-1:0] timer_value,
   output logic                  interrupt
 );
+  
   logic [TIMR_WIDTH-1:0] timer_count;
-
+  
+  always_ff @(posedge clk) begin
+    if (reset) begin
+      timer_count <= {TIMR_WIDTH{1'b1}};
+    end else if (timer_start) begin
+      timer_count <= timer_value;
+    end else if (|timer_count) begin
+      timer_count <= timer_count - 1'b1;
+    end
+  end
+  // live-interrupt to interrupt controller
+  always_ff @(posedge clk) begin
+    interrupt <= ~(|timer_count);
+  end
+  
 endmodule
