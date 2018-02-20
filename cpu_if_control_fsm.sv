7495f61d6cd31027440d890648da00056aa42203
Jomon K Joy <jomonkurianjoy@gmail.com>
2018-02-20 19:04:12 +0000

    Update cpu_if_control_fsm.sv

--- cpu_if_control_fsm.sv
+++ cpu_if_control_fsm.sv
@@ -1,76 +1,38 @@
+// minimum logic state machine 
 module cpu_if_control_fsm (
     input  logic clk, 
     input  logic reset, 
     input  logic read, 
     input  logic write, 
     input  logic access_complete,
-    output logic ready,
-    output logic read_busy,
-    output logic write_busy
+    output logic access_ready
 );
 
-enum int unsigned { IDLE=0, RD_ACTIVE=1, WR_ACTIVE=2 } state = IDLE;
-
-// ready logic
-always_ff @(posedge clk) begin
-    if (reset) begin
-        ready <= 1'b1;
-    end else if (state == IDLE) begin
-        ready <= 1'b1;
-    end else begin
-        ready <= 1'b0;
-    end
-end
-
-// busy when read logic
-always_ff @(posedge clk) begin
-    if (reset) begin
-        read_busy <= 1'b0;
-    end else if (state == RD_ACTIVE) begin
-        read_busy <= 1'b1;
-    end else begin
-        read_busy <= 1'b0;
-    end
-end
-
-// busy when write logic
-always_ff @(posedge clk) begin
-    if (reset) begin
-        write_busy <= 1'b0;
-    end else if (state == WR_ACTIVE) begin
-        write_busy <= 1'b1;
-    end else begin
-        write_busy <= 1'b0;
-    end
-end
+enum int unsigned { IDLE=0, ACTIVE=1 } state = IDLE;
 
 // state-machine defenition
 always_ff @(posedge clk) begin
     if (reset) begin
         state <= IDLE;
+        access_ready <= 1'b1;
     end else begin
         case (state)
             IDLE: begin
-                if (write) begin
-                    state <= WR_ACTIVE;
-                end else if (read) begin
-                    state <= RD_ACTIVE;
+                if (write|read) begin
+                    state <= ACTIVE;
+                    access_ready <= 1'b0;
                 end else begin
                     state <= IDLE;
+                    access_ready <= 1'b1;
                 end
             end
-            WR_ACTIVE: begin
+            ACTIVE: begin
                 if (access_complete) begin
                     state <= IDLE;
+                    access_ready <= 1'b1;
                 end else begin
-                    state <= WR_ACTIVE;
-                end
-            end
-            RD_ACTIVE: begin
-                if (access_complete) begin
-                    state <= IDLE;
-                end else begin
-                    state <= RD_ACTIVE;
+                    state <= ACTIVE;
+                    access_ready <= 1'b0;
                 end
             end
             default: begin
