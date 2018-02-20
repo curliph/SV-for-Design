5e1b50b6b2bf04913dbbaa3ed9c04d1463ee6fef
Jomon K Joy <jomonkurianjoy@gmail.com>
2018-02-20 19:07:54 +0000

    Update cpu_if_control_fsm.sv

--- cpu_if_control_fsm.sv
+++ cpu_if_control_fsm.sv
@@ -1,46 +1,46 @@
 // minimum logic state machine 
 module cpu_if_control_fsm (
-    input  logic clk, 
-    input  logic reset, 
-    input  logic read, 
-    input  logic write, 
-    input  logic access_complete,
-    output logic access_ready
+  input  logic clk, 
+  input  logic reset, 
+  input  logic read, 
+  input  logic write, 
+  input  logic access_complete,
+  output logic access_ready
 );
+  
+  enum int unsigned { IDLE=0, ACTIVE=1 } state = IDLE;
 
-enum int unsigned { IDLE=0, ACTIVE=1 } state = IDLE;
-
-// state-machine defenition
-always_ff @(posedge clk) begin
+  // state-machine defenition
+  always_ff @(posedge clk) begin
     if (reset) begin
-        state <= IDLE;
-        access_ready <= 1'b1;
+      state <= IDLE;
+      access_ready <= 1'b1;
     end else begin
-        case (state)
-            IDLE: begin
-                if (write|read) begin
-                    state <= ACTIVE;
-                    access_ready <= 1'b0;
-                end else begin
-                    state <= IDLE;
-                    access_ready <= 1'b1;
-                end
-            end
-            ACTIVE: begin
-                if (access_complete) begin
-                    state <= IDLE;
-                    access_ready <= 1'b1;
-                end else begin
-                    state <= ACTIVE;
-                    access_ready <= 1'b0;
-                end
-            end
-            default: begin
-                state <= IDLE;
-                $display ("Reach undefined state");
-            end
-        endcase
+      case (state)
+        IDLE: begin
+          if (write|read) begin
+            state <= ACTIVE;
+            access_ready <= 1'b0;
+          end else begin
+            state <= IDLE;
+            access_ready <= 1'b1;
+          end
+        end
+        ACTIVE: begin
+          if (access_complete) begin
+            state <= IDLE;
+            access_ready <= 1'b1;
+          end else begin
+            state <= ACTIVE;
+            access_ready <= 1'b0;
+          end
+        end
+        default: begin
+          state <= IDLE;
+          $display ("Reach undefined state");
+        end
+      endcase
     end
-end
-
+  end
+  
 endmodule
