6dae48f319a0de61495bb79cf5831d6b89c2de38
Jomon K Joy <jomonkurianjoy@gmail.com>
2018-02-22 15:56:49 +0000

    Update reset_synchronizer.sv

--- reset_synchronizer.sv
+++ reset_synchronizer.sv
@@ -1,7 +1,7 @@
 /////////////////////////////////////////////////////////
 // Synnchronize for Async reset assert and Sync deassert
 /////////////////////////////////////////////////////////
-module reset_synchronizer #(
+module async_reset_synchronizer #(
   parameter SYNC_STAGE = 4
 ) (
   input  logic clk,
@@ -24,3 +24,40 @@ module reset_synchronizer #(
   assign reset_sync_n = !reset[SYNC_STAGE-1];
   
 endmodule
+
+module async_sync_reset_synchronizer #(
+  parameter ASYNC_STAGE = 5,
+  parameter SYNC_STAGE = 2
+) (
+  input  logic clk,
+  input  logic reset_async,
+  output logic reset_sync,
+  output logic reset_sync_n
+);
+  
+  logic [ASYNC_STAGE-1:0] reset_async_reg = {ASYNC_STAGE{1'b1}};
+  logic [SYNC_STAGE-1:0] reset_sync_reg = {SYNC_STAGE{1'b1}};
+  // async assert sync deassert using async-register
+  // mitigate reset recovery and reset removal of deassertion of async-reset input 
+  always_ff @(posedge clk or posedge reset_async) begin
+    if (reset_async) begin
+      reset_async_reg <= {ASYNC_STAGE{1'b1}};
+    end else begin
+      reset_async_reg <= {reset_async_reg[ASYNC_STAGE-2:0],1'b0};
+    end
+  end
+  // sync assert sync deassert using sync-register
+  // mitigate the CDC-syncronization of sync-reset output
+  always_ff @(posedge clk) begin
+    if (reset_async_reg[ASYNC_STAGE-1]) begin
+      reset_sync_reg <= {SYNC_STAGE{1'b1}};
+    end else begin
+      reset_sync_reg <= {reset_sync_reg[SYNC_STAGE-2:0],1'b0};
+    end
+  end
+  
+  assign reset_sync   =  reset_sync_reg[SYNC_STAGE-1];
+  assign reset_sync_n = !reset_sync_reg[SYNC_STAGE-1];
+  
+endmodule
+
