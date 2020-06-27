ec7bfc97cb38c3bdee7fb066e58aa20d2c548575
Design@vlsi <jomonkurianjoy@gmail.com>
2020-06-27 15:51:38 +0530

    Update freq_divider.sv

--- freq_divider.sv
+++ freq_divider.sv
@@ -17,13 +17,14 @@ localparam COUNT_BIT = $clog2(COUNT_MAX);
 
 logic clk_out_q = 1'b0;
 logic [COUNT_BIT-1:0] count = {COUNT_BIT{1'b0}};
+logic end_of_count;
 
 assign clk_out = clk_out_q;
 
 always_ff @( posedge clk_in ) begin
     if ( reset ) begin
         count <= {COUNT_BIT{1'b0}};
-    end else if ( count >= COUNT_MAX-1 ) begin
+    end else if ( end_of_count ) begin
         count <= {COUNT_BIT{1'b0}};
     end else begin
         count <= count + 1'b1;
@@ -31,9 +32,13 @@ always_ff @( posedge clk_in ) begin
 end
 
 always_ff @( posedge clk_in ) begin
+    end_of_count <= count == COUNT_MAX-2;
+end
+
+always_ff @( posedge clk_in ) begin
     if ( reset ) begin
         clk_out_q <= 1'b0;
-    end else if ( count == COUNT_MAX-1 ) begin
+    end else if ( end_of_count ) begin
         clk_out_q <= ~clk_out_q;
     end
 end
